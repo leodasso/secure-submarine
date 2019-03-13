6b15aa5c832845765dc8c2d5866353a5fc2d7591
Leo Dasso <leo.dasso@gmail.com>
2019-03-13 15:44:27 -0500

    initial commit

---
+++ server/routes/secrets.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('req.user:', req.user);
+    pool.query('SELECT * FROM "secret";')
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for secrets:', error);
+            res.sendStatus(500);
+        });
+});
+
+module.exports = router;
